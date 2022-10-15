d16c97d87045af88ba2916cc3acede37d5e42254
shubhamp861 <46345008+shubhamp861@users.noreply.github.com>
2022-10-15 11:34:08 +0530

    Update user.service.ts

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   constructor(private http:HttpClient) { }
  
  // baseURL = "http://localhost:8080/user/v1";
-  baseURL = " https://app-quiz-bck.herokuapp.com/user/v1";
+  baseURL = "https://app-quiz-bck.herokuapp.com/user/v1";
   createUser(user:any){
     return this.http.post(`${this.baseURL}/create`,user);
   }
